0aef628baff96788b93a17d7d3c1d2b45a77ef53
Darius Runge <darius-runge@magenta.de>
2023-08-19 19:54:22 +0200

    Moved 'test' dir to 'src'

--- src/test/circular-buffer/test.sh
+++ src/test/circular-buffer/test.sh
@@ -6,6 +6,6 @@ script_file=$(basename "$0")
 for file in $files; do
   if [ -f "$file" ] && [ "$file" != "$script_file" ]; then
     echo "Running test for $file"
-    gcc "$file" ../../circular-buffer.c -g -O3 -fanalyzer && valgrind -q ./a.out && rm a.out
+    gcc "$file" ../../src/circular-buffer.c -g -O3 -fanalyzer && valgrind -q ./a.out && rm a.out
   fi
 done
